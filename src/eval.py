8377a7a489a4ccfac40de7cddbe8f967c849d717
Sazuna <liza.fretel@gmail.com>
2023-10-02 11:17:26 +0200

    Clean eval.py

--- src/eval.py
+++ src/eval.py
@@ -1,24 +1,8 @@
 #!/bin/python3
 import sys
 
-"""
-fref, fhyp = sys.argv[1], sys.argv[2]
-tokscmp = []
-sentscmp = []
-with open(fref) as refsents, open(fhyp) as hypsents:
-	for refsent, hypsent in zip(refsents, hypsents):
-		print(refsent, hypsent)
-		for reftok, hyptok in zip(refsent, hypsent):
-			tokscmp.append(reftok == hyptok)
-		sentscmp.append(refsent == hypsent)
-print(sum(tokscmp), len(tokscmp), sum(tokscmp)/len(tokscmp))
-print(sum(sentscmp), len(sentscmp), sum(sentscmp)/len(sentscmp))
-"""
 def evaluate(predictions, labels):
-	print(predictions)
-	print(labels)
 	predlabels = list(zip(predictions.split(' '), labels))
-	print(predlabels)
 	errors = 0
 	correct = 0
 	for predlabel in predlabels:
