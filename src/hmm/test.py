d4181af99d18a524264865c64bb93031b40647ea
Sazuna <liza.fretel@gmail.com>
2023-10-09 12:23:50 +0200

    No Transition on pospos table bug fixed

--- src/hmm/test.py
+++ src/hmm/test.py
@@ -28,13 +28,13 @@ with open("../freqpospos.json", 'r') as f:
 df_pos_pos = pandas.DataFrame.from_dict(freqpospos)
 
 df_pos_pos = df_pos_pos.fillna(0)
-df_pos_pos = df_pos_pos.T
+df_pos_pos = df_pos_pos
 
 p_trans = df_pos_pos / df_pos_pos.sum()
 
 # quelle est la probabilité de cette phrase si on a les POS suivants ?
-phrase = "Le président gagne ."
-pos = "DET NOUN VERB PUNCT"
+phrase = "le président gagne"
+pos = "DET NOUN VERB"
 
 tok_lst = phrase.split(' ')
 pos_lst = pos.split(' ')
@@ -42,8 +42,9 @@ prevpos = '_'
 proba_sent = 1
 for tok, pos in zip (tok_lst, pos_lst):
 	p_gen_ = p_gen[pos][tok]
-	p_trans_ = p_trans[pos][prevpos]#[prevpos][pos]
+	p_trans_ = p_trans[prevpos][pos]#[prevpos][pos]
 	proba_sent *= p_gen_ * p_trans_
+	print(p_gen_, p_trans_, proba_sent)
 	prevpos = pos
 
 print("proba sent:", proba_sent)
